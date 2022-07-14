1542470a4aa0c6db35343b0ca05d6e553209d2f4
SeverinoAmmirati <sev.amm.dev@gmail.com>
2022-07-14 15:57:18 +0200

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import elements from './words.json';
+
 
 function App() {
+  const [image, setImage] = useState<string>("");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <img src={image} alt="" />
+
+        <div>
+            <button onClick={() => {
+            const random = Math.floor(Math.random() * elements.elements.length - 1);
+            const path = process.env.PUBLIC_URL + "images/" + elements.elements[random]
+            setImage(path)
+        }}>Genera parola
+            </button>
+        </div>
+
+
     </div>
+
   );
 }
 
